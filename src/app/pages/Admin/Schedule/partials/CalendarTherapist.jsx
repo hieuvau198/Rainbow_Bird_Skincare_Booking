396616e4daf5be3ef163ddc36333ed1236f235e1
hieuvau198 <nhatduongfpt2000@gmail.com>
2025-03-31 20:38:23 +0700

    fix/therapist-schedule-1

--- src/app/pages/Admin/Schedule/partials/CalendarTherapist.jsx
+++ src/app/pages/Admin/Schedule/partials/CalendarTherapist.jsx
@@ -1,28 +1,27 @@
+// pages/Admin/Schedule/partials/CalendarTherapist.jsx
+// this page is for therapist role to see only
+// it helps therapist to view the calendar and specific info like slot we assign to work in, bookings
+
 import { Calendar, Tag } from 'antd';
 import React from 'react';
 import StatusColor from '../../../../components/StatusColor';
 
-export default function CalendarTherapist({ bookings }) {
+export default function CalendarTherapist({ slots }) {
+  // Extract all working dates with at least one slot
+  const slotDatesSet = new Set(slots.map(slot => slot.workingDate));
+
   const cellRender = (current, info) => {
     if (info.type === 'date') {
       const dateStr = current.format('YYYY-MM-DD');
-      const dayBookings = bookings.filter(
-        (booking) =>
-          booking.bookingDate === dateStr &&
-          booking.status === "In Progress"
-      );
+      const hasSlot = slotDatesSet.has(dateStr);
+
       return (
-        <div className="relative">
+        <div
+          className={`relative rounded-sm transition-all duration-200 ${
+            hasSlot ? 'bg-lime-100 border border-lime-400' : ''
+          }`}
+        >
           {info.originNode}
-          {dayBookings.length > 0 && (
-            <ul className="mt-1 space-y-1">
-              {dayBookings.map((item) => (
-                <li key={item.bookingId}>
-                  <Tag color={StatusColor(item.status)}>Service ID: {item.serviceId}</Tag>
-                </li>
-              ))}
-            </ul>
-          )}
         </div>
       );
     }
@@ -31,6 +30,7 @@ export default function CalendarTherapist({ bookings }) {
 
   return (
     <div className="bg-white p-6 rounded-md shadow-md h-full">
+      <h2 className="text-lg font-semibold mb-4 text-gray-700">Therapist Working Schedule</h2>
       <Calendar cellRender={cellRender} fullscreen />
     </div>
   );
